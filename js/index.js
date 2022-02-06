09d0663d44690e20c8c41906c0b2621c643de59e
Lizaveta01 <goldglaider@gmail.com>
2022-02-06 16:02:57 +0300

    feat: add progress bar

--- js/index.js
+++ js/index.js
@@ -1,21 +1,114 @@
 const audio = document.querySelector('audio');
 const playBtn = document.querySelector('.play-btn');
 const pauseBtn = document.querySelector('.pause-btn');
+const nextBtn = document.querySelector('.next-btn');
+const lastBtn = document.querySelector('.last-btn');
+const imageTreck = document.getElementById('poster');
+const imageBack = document.getElementById('background');
+const nameSong = document.querySelector('.info-song__title');
+const nameArtist = document.querySelector('.info-song__subtitle');
+const progressContainer = document.querySelector('.progress__container');
+const progress = document.querySelector('.progress');
+
 let isPlay = false;
+let playNum = 0;
+const playList = ['demons', 'september'];
+const listSong = ['Demons', 'September'];
+const listArtist =['Imagine Dragons', 'Earth wind fire'];
+
+
+
+function switcherTreck(playNum) {
+	audio.src = `/assets/song/${playList[playNum]}.mp3`;
+	isPlay = false;
+	switcherImage();
+	playAudio();	
+	switcherText()
+}
+
+function switcherText() {
+	console.log('работает2!');
+	nameSong.textContent = listSong[playNum];
+	nameArtist.textContent = listArtist[playNum];
+}
+
+function switcherImage() {
+	console.log('работает!');
+	imageTreck.src = `/assets/img/jpg/${playList[playNum]}.jpg`;
+	imageBack.src = `/assets/img/jpg/${playList[playNum]}.jpg`;
+}
+
+function switcherImage() {
+	console.log('работает!');
+	imageTreck.src = `/assets/img/jpg/${playList[playNum]}.jpg`;
+	imageBack.src = `/assets/img/jpg/${playList[playNum]}.jpg`;
+}
+
+function playNext() {
+	if (playNum >= playList.length -1) {
+		playNum = 0;
+	} else {
+		playNum++
+	}
+	switcherTreck(playNum);	
+	console.log(playNum);
+}
+nextBtn.addEventListener('click', playNext);
+
+
+function playPrev() {
+	if (playNum > 0) {
+		playNum--
+	} else {
+		playNum = playList.length - 1;
+	}	
+	switcherTreck(playNum);
+}
+lastBtn.addEventListener('click', playPrev);
 
 function playAudio() {
   if (!isPlay) {
 	audio.play();
 	isPlay = true;
+	playBtn.classList.add('pause-btn')
+	imageTreck.classList.add('active');
   } else {
 	audio.pause();
 	isPlay = false;
+	playBtn.classList.remove('pause-btn')
   }
 }
 
 playBtn.addEventListener('click', playAudio);
 
-function toggleBtn() {
-	playBtn.classList.toggle('pause-btn');
-  }
-  playBtn.addEventListener('click', toggleBtn);
+
+function changeImage(event) {
+	if (event.target.classList.contains('bt__border')) {
+		console.log(event.target);
+		portfolioImages.forEach((img, index) => {
+			img.src = `assets/img/jpg/${event.target.dataset.season}/${index + 1}.jpg`
+		});
+	}
+}
+
+//плеер
+function updateProgress(e) {
+	const {duration, currentTime} = e.srcElement;
+	const progressPercent = (currentTime / duration) * 100;
+	progress.style.width = `${progressPercent}%`;
+	console.log('работает3!')
+}
+
+audio.addEventListener('timeupdate', updateProgress);
+
+//перемотка
+function setProgress (e){
+	const width = this.clientWidth;
+	const clickX = e.offsetX;
+	const duration = audio.duration;
+	audio.currentTime = (clickX / width) * duration;
+}
+progressContainer.addEventListener('click', setProgress);
+
+//автовоспроизведение следующей песни
+audio.addEventListener('ended', playNext);
