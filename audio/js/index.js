d7d7db2cc9b9df0e460bee0d72b5c7055c61487e
Lizaveta Petrova <81522781+Lizaveta01@users.noreply.github.com>
2022-02-07 13:33:42 +0300

    update 1

--- audio/js/index.js
+++ audio/js/index.js
@@ -22,7 +22,7 @@ const listArtist =['Imagine Dragons', 'Earth wind fire'];
 
 
 function switcherTreck(playNum) {
-	audio.src = `assets/song/${playList[playNum]}.mp3`;
+	audio.src = `/audio/assets/song/${playList[playNum]}.mp3`;
 	isPlay = false;
 	switcherImage();
 	playAudio();	
@@ -37,8 +37,8 @@ function switcherText() {
 
 function switcherImage() {
 	console.log('работает!');
-	imageTreck.src = `assets/img/jpg/${playList[playNum]}.jpg`;
-	imageBack.src = `assets/img/jpg/${playList[playNum]}.jpg`;
+	imageTreck.src = `/audio/assets/img/jpg/${playList[playNum]}.jpg`;
+	imageBack.src = `/audio/assets/img/jpg/${playList[playNum]}.jpg`;
 }
 
 
